835fe3ee35b4a0d818ab6889761ecc42677303fb
Thamires Garcia <thamiresgarcia@Thamiress-MBP.Home>
2023-01-24 10:04:11 +0000

    infra(initial): added custom theme to storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import { setCompodocJson } from "@storybook/addon-docs/angular";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: { inlineStories: true },
+}
