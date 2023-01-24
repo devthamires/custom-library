228b1005efcba4a16d73b23e2fe894e9f0748182
Thamires Garcia <thamiresgarcia@Thamiress-MBP.Home>
2023-01-24 13:06:51 +0000

    infra(naming): change naming

---
+++ projects/ui-library/src/lib/ui-library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui-library',
+  template: `
+    <p>
+      ui-library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UiLibraryComponent {
+
+}
---
+++ projects/ui-library/src/lib/ui-library.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { UiLibraryComponent } from './ui-library.component';
+
+
+
+@NgModule({
+  declarations: [
+    UiLibraryComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    UiLibraryComponent
+  ]
+})
+export class UiLibraryModule { }
